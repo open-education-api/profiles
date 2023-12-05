064ed3865da049a544ae082907dc3d7690814c60
Herman van Dompseler <herman.vandompseler@surf.nl>
2023-12-05 18:35:10 +0100

    add first eduxchange profile and build script

--- build.sh
+++ build.sh
@@ -1,2 +1,27 @@
 #! /bin/bash
 
+PROFILE=$1
+
+# check input
+if [ ! -d $PROFILE ] || [ "$PROFILE" == "" ]; then
+    echo "profile \"$PROFILE\" is not a directory"
+    echo "usage: build.sh PROFILE"
+    exit 1;
+fi
+
+# clear target
+rm -rf target
+mkdir target
+
+# merge
+echo "merge profile...";
+./merge-yaml-tree base/v5 $PROFILE/profile target spec.yaml
+
+# create
+echo "create json...";
+cd target
+npx @redocly/openapi-cli bundle --ext=json spec.yaml > ../$PROFILE/$PROFILE.json
+cd ..
+
+echo "validate...\n";
+./eduhub-validator -r $PROFILE/rules.edn -u https://demo04.test.surfeduhub.nl -o observations.edn -p report.html
