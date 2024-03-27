5783daf0e4cde95360d5f16278a2b64f073fc47b
Herman van Dompseler <herman.vandompseler@surf.nl>
2024-03-27 15:51:21 +0100

    fix oneof issue and comply to latest consumer

--- build.sh
+++ build.sh
@@ -10,18 +10,20 @@ if [ ! -d $PROFILE ] || [ "$PROFILE" == "" ]; then
 fi
 
 # clear target
-rm -rf target
-mkdir target
+rm -rf $PROFILE/target
+mkdir $PROFILE/target
 
 # merge
 echo "merge profile...";
-./merge-yaml-tree base/v5 $PROFILE/profile target spec.yaml
+./merge-yaml-tree base/v5 $PROFILE/profile $PROFILE/target spec.yaml
 
 # create
 echo "create json...";
-cd target
-npx @redocly/openapi-cli bundle --ext=json spec.yaml > ../$PROFILE/$PROFILE.json
-cd ..
+cd $PROFILE/target
+npx @redocly/openapi-cli bundle --ext=json spec.yaml > ../$PROFILE.json
+cd ../..
 
 echo "validate...\n";
+rm observations.edn
+rm report.html
 ./eduhub-validator -r $PROFILE/rules.edn -u https://demo04.test.surfeduhub.nl -o observations.edn -p report.html
